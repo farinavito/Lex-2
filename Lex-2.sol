7733776df7935cc274afbedb01956c9b20f25b07
Vito <farinavito@gmail.com>
2022-03-12 08:40:03 +0100

    added spdx, title, author

--- Lex-2.sol
+++ Lex-2.sol
@@ -1,1 +1,5 @@
-//start...
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+
+/// @title Implementing a legal contract: Person A commits sending X amount to person B until Y date.
+/// @author Farina Vito
