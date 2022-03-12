fbf18c8ae67d97888efbdb5e482001995d91efa6
Vito <farinavito@gmail.com>
2022-03-12 08:41:33 +0100

    added a contract

--- Lex-2.sol
+++ Lex-2.sol
@@ -3,3 +3,7 @@ pragma solidity 0.8.11;
 
 /// @title Implementing a legal contract: Person A commits sending X amount to person B until Y date.
 /// @author Farina Vito
+
+contract sendMoneyUntil {
+    
+}
