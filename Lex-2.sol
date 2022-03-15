ffb7deb6d811c379c5d38677ff4af94fb7d23539
Vito <farinavito@gmail.com>
2022-03-15 23:47:10 +0100

    added uncomplited functionality to createAgreement

--- Lex-2.sol
+++ Lex-2.sol
@@ -168,6 +168,39 @@ contract sendMoneyUntil {
         require(exactAgreement[_id].deadline > block.timestamp, "The agreement's deadline has ended");
         require(exactAgreement[_id].amount <= msg.value, "The amount sent isn't equal to the contract's amount");
 
+        if (timeNotBreached(_id)){
+          if (exactAgreement[_id].amount <= msg.value){
+            //storing the amount sent subtracted by commission
+            uint256 changedAmount;
+            changedAmount = msg.value - commission;
+            //adding the commission to a owner's withdrawal
+            withdrawal_amount_owner += commission;
+            //send the transaction to the receiver
+            withdraw_receiver[exactAgreement[_id].receiver] += changedAmount;
+            emit NotifyUser("Transaction was sent to the receiver");
+          //if the transaction was on time, but it wasn't enough
+          } else {
+              exactAgreement[_id].status = "Terminated"; 
+              //sending the deposit to the receiver
+              withdraw_signee[exactAgreement[_id].signee] += exactAgreement[_id].deposit;
+              //ensure that the deposit is reduced to 0
+              exactAgreement[_id].deposit = 0;
+              //return the transaction to the signee
+              withdraw_signee[exactAgreement[_id].signee] += msg.value;
+              emit Terminated("The agreement was terminated due to different amount sent than in the terms");      
+          }
+        //if the transaction wasn't sent on time
+        } else {
+          exactAgreement[_id].status = "Terminated";
+          //sending the deposit to the receiver
+          withdraw_receiver[exactAgreement[_id].receiver] += exactAgreement[_id].deposit;
+          //ensure that the deposit is reduced to 0
+          exactAgreement[_id].deposit = 0;
+          //return the transaction to the signee
+          withdraw_signee[exactAgreement[_id].signee] += msg.value;
+          emit Terminated("The agreement was terminated due to late payment");
+        }
+
         //storing the amount sent subtracted by commission
         uint256 changedAmount;
         changedAmount = msg.value - commission;
