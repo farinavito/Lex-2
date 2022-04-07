7ba104639957c588b77c69ef4297841f8d8d0132
Vito <farinavito@gmail.com>
2022-04-07 09:51:29 +0200

    added a comment and changed the position in the contract

--- project/contracts/Lex-2.sol
+++ project/contracts/Lex-2.sol
@@ -9,141 +9,142 @@ pragma solidity ^0.8.11;
 
 //this contract is added only for testing purposes of AgreementBetweenSubjects
 contract AddressProtector {
+  
+  /// @notice Adding votes for candidates by protectors
+  mapping (address => mapping(address => bool)) public alreadyVoted;
+  
+  /// @notice Candidate for protectorWaitingToBeOwner
+  mapping (address => uint256) public candidatesVotes;
 
-    /// @notice Adding votes for candidates by protectors
-    mapping (address => mapping(address => bool)) public alreadyVoted;
-    
-    /// @notice Candidate for protectorWaitingToBeOwner
-    mapping (address => uint256) public candidatesVotes;
+  /// @notice Whitelisted accounts that can access withdrawal_amount_owner
+  mapping(address => bool) public whitelist;
+      
+  /// @notice Storing the owner's address
+  address public smartcontractOwner;
 
-    /// @notice Whitelisted accounts that can access withdrawal_amount_owner
-    mapping(address => bool) public whitelist;
-        
-    /// @notice Storing the owner's address
-    address public smartcontractOwner;
+  /// @notice Storing the next in line to be an owner
+  address public protectorWaitingToBeOwner;
 
-    /// @notice Storing the next in line to be an owner
-    address public protectorWaitingToBeOwner;
+  ///@notice Storing all protectors
+  address[] internal allprotectorsaddresses;
 
-    ///@notice Storing all protectors
-    address[] internal allprotectorsaddresses;
-
-    /// @notice Emit all the addresses of the protectors
-    event showAllProtectors(address indexed _address);
+  /// @notice Emit all the addresses of the protectors
+  event showAllProtectors(address indexed _address);
 
 
-    constructor (
-        address _protectorOwner,
-        address _protectorWaitingToBeOwner, 
-        address _protector1, 
-        address _protector2, 
-        address _protector3, 
-        address _protector4, 
-        address _protector5 
-        ){
-        smartcontractOwner = _protectorOwner;
-        protectorWaitingToBeOwner = _protectorWaitingToBeOwner;
+  constructor (
+      address _protectorOwner,
+      address _protectorWaitingToBeOwner, 
+      address _protector1, 
+      address _protector2, 
+      address _protector3, 
+      address _protector4, 
+      address _protector5 
+      ){
+      smartcontractOwner = _protectorOwner;
+      protectorWaitingToBeOwner = _protectorWaitingToBeOwner;
 
-        allprotectorsaddresses.push(_protector1);
-        allprotectorsaddresses.push(_protector2);
-        allprotectorsaddresses.push(_protector3);
-        allprotectorsaddresses.push(_protector4);
-        allprotectorsaddresses.push(_protector5);
+      allprotectorsaddresses.push(_protector1);
+      allprotectorsaddresses.push(_protector2);
+      allprotectorsaddresses.push(_protector3);
+      allprotectorsaddresses.push(_protector4);
+      allprotectorsaddresses.push(_protector5);
 
-        //initialize the protectors
-        for (uint8 i = 1; i <= 5; i++){
-            candidatesVotes[protectorWaitingToBeOwner] += 1;
-            alreadyVoted[allprotectorsaddresses[i - 1]][protectorWaitingToBeOwner] = true;
-        }
-    }
+      //initialize the protectors
+      for (uint8 i = 1; i <= 5; i++){
+          candidatesVotes[protectorWaitingToBeOwner] += 1;
+          alreadyVoted[allprotectorsaddresses[i - 1]][protectorWaitingToBeOwner] = true;
+      }
+  }
 
-    /// @notice Checking if the input address is the protector
-    function checkWhichProtector(address _address) internal view returns(uint8 _i){
-        for (uint8 i = 0; i < 5; i++){
-            if (allprotectorsaddresses[i] == _address){
-                return i;
-            } else if (i != 4){
-                continue;
-            } else {
-                revert("You don't have permissions");
-            }
-        }
-    }
+  /// @notice Checking if the input address is the protector
+  function checkWhichProtector(address _address) internal view returns(uint8 _i){
+      for (uint8 i = 0; i < 5; i++){
+          if (allprotectorsaddresses[i] == _address){
+              return i;
+          } else if (i != 4){
+              continue;
+          } else {
+              revert("You don't have permissions");
+          }
+      }
+  }
 
-    /// @notice Returning all addresses of protectors
-    function returnProtectors() public {
-        for (uint8 i = 0; i < 5; i++){
-            emit showAllProtectors(allprotectorsaddresses[i]);
-        }
-    }
+  /// @notice Returning all addresses of protectors
+  function returnProtectors() public {
+      for (uint8 i = 0; i < 5; i++){
+          emit showAllProtectors(allprotectorsaddresses[i]);
+      }
+  }
 
-    /// @notice Changing the owner and the waitingToBeOwner
-    function changeOwner(address _nextInline) external {
-        require(protectorWaitingToBeOwner == msg.sender, "You don't have permissions");
-        require(candidatesVotes[_nextInline] >= 3, "Not all protectors agree with this address");
-        //reinitializing to 0
-        candidatesVotes[smartcontractOwner] = 0;
-        for (uint8 i = 0; i < 5; i++){
-            alreadyVoted[allprotectorsaddresses[i]][smartcontractOwner] = false;
-        }
+  /// @notice Changing the owner and the waitingToBeOwner
+  function changeOwner(address _nextInline) external {
+      require(protectorWaitingToBeOwner == msg.sender, "You don't have permissions");
+      require(candidatesVotes[_nextInline] >= 3, "Not all protectors agree with this address");
+      //reinitializing to 0
+      candidatesVotes[smartcontractOwner] = 0;
+      for (uint8 i = 0; i < 5; i++){
+          alreadyVoted[allprotectorsaddresses[i]][smartcontractOwner] = false;
+      }
 
-        smartcontractOwner = protectorWaitingToBeOwner;
-        protectorWaitingToBeOwner = _nextInline;
-    }
-    
-    /// @notice Voting for candidates by protectors
-    function voteCandidate(address _nextInLine) external {
-        checkWhichProtector(msg.sender);
-        require(alreadyVoted[msg.sender][_nextInLine] == false, "You have entered your vote");
-        alreadyVoted[msg.sender][_nextInLine] = true;
-        candidatesVotes[_nextInLine] += 1;
-    }
+      smartcontractOwner = protectorWaitingToBeOwner;
+      protectorWaitingToBeOwner = _nextInline;
+  }
+  
+  /// @notice Voting for candidates by protectors
+  function voteCandidate(address _nextInLine) external {
+      checkWhichProtector(msg.sender);
+      require(alreadyVoted[msg.sender][_nextInLine] == false, "You have entered your vote");
+      alreadyVoted[msg.sender][_nextInLine] = true;
+      candidatesVotes[_nextInLine] += 1;
+  }
 
-    /// @notice remove vote by the protector from previously voted protectorWaitingToBeOwner
-    function removeVote(address _nextInLine) external {
-        checkWhichProtector(msg.sender);
-        require(alreadyVoted[msg.sender][_nextInLine] == true, "You haven't voted for this address");
-        alreadyVoted[msg.sender][_nextInLine] = false;
-        candidatesVotes[_nextInLine] -= 1;
-    }
+  /// @notice remove vote by the protector from previously voted protectorWaitingToBeOwner
+  function removeVote(address _nextInLine) external {
+      checkWhichProtector(msg.sender);
+      require(alreadyVoted[msg.sender][_nextInLine] == true, "You haven't voted for this address");
+      alreadyVoted[msg.sender][_nextInLine] = false;
+      candidatesVotes[_nextInLine] -= 1;
+  }
 
-    /// @notice Only the protectorOwner can access
-    modifier onlyprotectorOwner(){
-        require(msg.sender == smartcontractOwner, "You are not the owner");
-        _;
-    }
+  /// @notice Only the protectorOwner can access
+  modifier onlyprotectorOwner(){
+      require(msg.sender == smartcontractOwner, "You are not the owner");
+      _;
+  }
 
-    /// @notice Adding address to the whitelist
-    function addToWhitelist(address _address) external onlyprotectorOwner {
-        whitelist[_address] = true;
-    }
-    
-    /// @notice Removing address from the whitelist
-    function removedFromWhitelist(address _address) external onlyprotectorOwner {
-        whitelist[_address] = false;
-    }
+  /// @notice Adding address to the whitelist
+  function addToWhitelist(address _address) external onlyprotectorOwner {
+      whitelist[_address] = true;
+  }
+  
+  /// @notice Removing address from the whitelist
+  function removedFromWhitelist(address _address) external onlyprotectorOwner {
+      whitelist[_address] = false;
+  }
     
 }
 
 contract sendMoneyUntil {
-    /// @notice Defining the agreement 
-    /// @param id A unique identifier of the agreement
-    /// @param signee The person who commits sending the money to the receiver 
-    /// @param receiver The person receiving the money
-    /// @param amount The quantity of money that the signee commits sending to the receiver
-    /// @param transactionCreated Unix timestamp when transaction was sent
-    /// @param deposit The first transaction sent to the agreement. Initial state will be zero
-    /// @param status Representation of different stages in the agreement: Created, Terminated
-    /// @param deadline The number of days till the agreement expires
-    struct Agreement{
-    uint256 id; 
-    address signee;
-    address payable receiver; 
-    uint256 amount;
-    uint256 deposit;
-    uint256 transactionCreated;
-    string status;
-    uint256 deadline;
+  //remove transactionCreated?
+  /// @notice Defining the agreement 
+  /// @param id A unique identifier of the agreement
+  /// @param signee The person who commits sending the money to the receiver 
+  /// @param receiver The person receiving the money
+  /// @param amount The quantity of money that the signee commits sending to the receiver
+  /// @param transactionCreated Unix timestamp when transaction was sent
+  /// @param deposit The first transaction sent to the agreement. Initial state will be zero
+  /// @param status Representation of different stages in the agreement: Created, Terminated
+  /// @param deadline The number of days till the agreement expires
+  struct Agreement{
+  uint256 id; 
+  address signee;
+  address payable receiver; 
+  uint256 amount;
+  uint256 deposit;
+  uint256 transactionCreated;
+  string status;
+  uint256 deadline;
   }
 
   /// @notice Storing the owner's address
@@ -168,11 +169,11 @@ contract sendMoneyUntil {
   /// @notice Returning the total amount of deposit that was sent to the receiver
   uint256 public totalDepositSent;  
 
-   modifier noReentrant() {
-    require(locked == 1, "No re-entrancy");
-    locked = 2;
-    _;
-    locked = 1;
+  modifier noReentrant() {
+  require(locked == 1, "No re-entrancy");
+  locked = 2;
+  _;
+  locked = 1;
   }
 
   /// @notice Saving the money sent for the signee to withdraw it
@@ -222,50 +223,50 @@ contract sendMoneyUntil {
     uint256 _amount,
     uint256 _deadline
     ) external payable {
-        require(_amount > 0 && _deadline > 0, "All input data must be larger than 0");
-        require(_deadline >= block.timestamp, "The agreement can't be created in the past");
-        //rule for the deposit -> min is 100 wei, if larger _amount, deposit is 10% of the _amount -> bp 10
-        
-        uint256 storeDeposit;
-        uint256 minDeposit = 100;
-        if (msg.value >= 1000){
-          //check if it works
-          storeDeposit = msg.value * 10 / 10000;
-        } else {
-          storeDeposit = minDeposit;
-        }
+      require(_amount > 0 && _deadline > 0, "All input data must be larger than 0");
+      require(_deadline >= block.timestamp, "The agreement can't be created in the past");
+      //rule for the deposit -> min is 100 wei, if larger _amount, deposit is 10% of the _amount -> bp 10
+      
+      uint256 storeDeposit;
+      uint256 minDeposit = 100;
+      if (msg.value >= 1000){
+        //check if it works
+        storeDeposit = msg.value * 10 / 10000;
+      } else {
+        storeDeposit = minDeposit;
+      }
 
-        require(msg.value >= storeDeposit, "Deposit needs to be 10% of the amount or at least 100 wei");
-        
-        //increment the agreement id
-        uint256 agreementId = numAgreement++;
-        //creating a new agreement
-        Agreement storage newAgreement = exactAgreement[agreementId];
-        newAgreement.id = agreementId;
-        newAgreement.signee = msg.sender;
-        newAgreement.receiver = _receiver;
-        newAgreement.amount = _amount;
-        newAgreement.deposit = storeDeposit;
+      require(msg.value >= storeDeposit, "Deposit needs to be 10% of the amount or at least 100 wei");
+      
+      //increment the agreement id
+      uint256 agreementId = numAgreement++;
+      //creating a new agreement
+      Agreement storage newAgreement = exactAgreement[agreementId];
+      newAgreement.id = agreementId;
+      newAgreement.signee = msg.sender;
+      newAgreement.receiver = _receiver;
+      newAgreement.amount = _amount;
+      newAgreement.deposit = storeDeposit;
 
-        //the status of the agreement when its created
-        newAgreement.status = "Created";
-        //how long will the agreement last
-        newAgreement.deadline = _deadline;
-        //storing the ids of the agreements and connecting them to msg.sender's address so we can display them to the frontend
-        mySenderAgreements[msg.sender].push(agreementId);
-        //storing the ids of the agreements and connecting them to _receiver's address so we can display them to the frontend
-        myReceiverAgreements[_receiver].push(agreementId);
+      //the status of the agreement when its created
+      newAgreement.status = "Created";
+      //how long will the agreement last
+      newAgreement.deadline = _deadline;
+      //storing the ids of the agreements and connecting them to msg.sender's address so we can display them to the frontend
+      mySenderAgreements[msg.sender].push(agreementId);
+      //storing the ids of the agreements and connecting them to _receiver's address so we can display them to the frontend
+      myReceiverAgreements[_receiver].push(agreementId);
 
-        emit AgreementInfo(
-          newAgreement.id, 
-          newAgreement.signee, 
-          newAgreement.receiver, 
-          newAgreement.amount,
-          newAgreement.deposit,
-          newAgreement.transactionCreated, 
-          newAgreement.status,
-          newAgreement.deadline
-          ); 
+      emit AgreementInfo(
+        newAgreement.id, 
+        newAgreement.signee, 
+        newAgreement.receiver, 
+        newAgreement.amount,
+        newAgreement.deposit,
+        newAgreement.transactionCreated, 
+        newAgreement.status,
+        newAgreement.deadline
+        ); 
   }
 
   /// @notice Sending the payment based on the status of the agreement
