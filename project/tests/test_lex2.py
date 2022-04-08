7e1b37f9bbb7f7ed49635fba3801cdd421db21ea
Vito <farinavito@gmail.com>
2022-04-08 18:15:41 +0200

    fixed test_withdrawAsTheSignee_withdrawal_sent_1

--- project/tests/test_lex2.py
+++ project/tests/test_lex2.py
@@ -499,7 +499,7 @@ def test_withdrawAsTheSignee_second_reguire_fails_case_1(deploy, time):
     '''require statement withdraw_receiver[exactAgreement[_id].signee] > 0 fails'''
     with brownie.reverts("There aren't any funds to withdraw"):
         deploy.withdrawAsTheSignee(agreements_number, {'from': accounts[signee]})
-@pytest.mark.aaa
+
 @pytest.mark.parametrize("time", [more_than_agreement_duration[0], more_than_agreement_duration[1], more_than_agreement_duration[2]])
 def test_withdrawAsTheSignee_second_reguire_fails_case_2(deploy, time):
     '''require statement withdraw_receiver[exactAgreement[_id].signee] > 0 fails'''
@@ -507,17 +507,14 @@ def test_withdrawAsTheSignee_second_reguire_fails_case_2(deploy, time):
     deploy.withdrawAsTheSignee(agreements_number, {'from': accounts[signee]})
     with brownie.reverts("There aren't any funds to withdraw"):
         deploy.withdrawAsTheSignee(agreements_number, {'from': accounts[signee]})
-
+@pytest.mark.aaa
 @pytest.mark.parametrize("time", [more_than_agreement_duration[0], more_than_agreement_duration[1], more_than_agreement_duration[2]])
 def test_withdrawAsTheSignee_withdrawal_sent_1(deploy, time):
     '''Check if the withdrawal is sent'''
     deploy.sendPayment(agreements_number, {'from': accounts[signee], 'value': amount_sent})
-    deploy.sendPayment(agreements_number, {'from': accounts[signee], 'value': 3*amount_sent})
     signee_balance = accounts[signee].balance()
-    chain = Chain()
-    chain.sleep(time)
     deploy.withdrawAsTheSignee(agreements_number, {'from': accounts[signee]})
-    assert accounts[signee].balance() == signee_balance + 2*amount_sent
+    assert accounts[signee].balance() == signee_balance + deposit
 
 @pytest.mark.parametrize("amount", [less_than_amount_sent[0], less_than_amount_sent[1], less_than_amount_sent[2]])
 def test_withdrawAsTheSignee_withdrawal_sent_2(deploy, amount):
