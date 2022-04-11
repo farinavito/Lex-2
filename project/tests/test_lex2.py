8e3cfb57481d641e0e44b9532588e6e02fdd11a7
Vito <farinavito@gmail.com>
2022-04-11 09:08:05 +0200

    added unmodified tests

--- project/tests/test_lex2.py
+++ project/tests/test_lex2.py
@@ -576,3 +576,27 @@ def test_withdrawAsTheOwner_check_event_emitted(deploy, deploy_addressProtector)
     function_initialize = deploy.withdrawAsTheOwner({'from': accounts[9]})
     assert function_initialize.events[0][0]['message'] == "Withdrawal has been transfered"  
 
+'''TEST GETWITHDRAWALRECEIVER'''
+
+
+
+@pytest.mark.parametrize("wrong_account", [without_receiver[0], without_receiver[1], without_receiver[2]])
+def test_getWithdrawalReceiver_reguire_fails(deploy, wrong_account):
+    '''require statement exactAgreement[_id].receiver == msg.sender fails'''
+    deploy.sendPayment(agreements_number, {'from': accounts[signee], 'value': amount_sent})
+    deploy.sendPayment(agreements_number, {'from': accounts[signee], 'value': 4*amount_sent})
+    with brownie.reverts("Your logged in address isn't the same as the agreement's receiver"):
+        deploy.getWithdrawalReceiver(agreements_number, {'from': accounts[wrong_account]})
+
+def test_getWithdrawalReceiver_reguire_fails_pair(deploy):
+    '''require statement exactAgreement[_id].receiver == msg.sender doesn't fail'''
+    deploy.sendPayment(agreements_number, {'from': accounts[signee], 'value': amount_sent})
+    deploy.sendPayment(agreements_number, {'from': accounts[signee], 'value': 4*amount_sent})
+    function_initialize = deploy.getWithdrawalReceiver(agreements_number, {'from': accounts[receiver]})
+    assert function_initialize == 4*amount_sent - commission
+
+def test_getWithdrawalReceiver_uninitialize(deploy):
+    '''check if the withdraw_receiver is empty after only sending the deposit'''
+    deploy.sendPayment(agreements_number, {'from': accounts[signee], 'value': amount_sent})
+    function_initialize = deploy.getWithdrawalReceiver(agreements_number, {'from': accounts[receiver]})
+    assert function_initialize == 0
